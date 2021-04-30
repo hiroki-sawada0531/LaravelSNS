a1fb1500c6c9682875ccebf90b785c1764d2df00
澤田大樹 <hiroki.bigtree@icloud.com>
2021-04-30 13:31:35 +0900

    全画面共通で使用するビューの新規作成 全画面共通で使用するビューの新規作成 全画面共通で使用するビューの新規作成

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index() {
+    $articles = [
+        (object) [
+            'id' => 1,
+            'title' => 'タイトル',
+            'body' => '本文',
+            'created_at' => now(),
+            'user' => (object) [
+                'id' => 1,
+                'name' => 'ユーザー名1',
+            ],
+        ],
+        (object) [
+            'id' => 2,
+            'title' => 'タイトル2',
+            'body' => '本文2',
+            'created_at' => now(),
+            'user' => (object) [
+                'id' => 2,
+                'name' => 'ユーザー名2',
+            ],
+        ],
+        (object) [
+            'id' => 2,
+            'title' => 'タイトル2',
+            'body' => '本文2',
+            'created_at' => now(),
+            'user' => (object) [
+                'id' => 2,
+                'name' => 'ユーザー名2',
+            ],
+        ],
+    ];
+
+    return view('articles.index',['articles' => $articles]);
+    }
 }

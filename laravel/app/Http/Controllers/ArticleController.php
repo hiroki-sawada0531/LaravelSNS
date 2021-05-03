386788fd57644ba6f284a0a78576050c52a81d3a
澤田大樹 <hiroki.bigtree@icloud.com>
2021-05-03 20:21:20 +0900

    記事更新画面のBladeを作成

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -23,10 +23,14 @@ class ArticleController extends Controller
 
     public function store(ArticleRequest $request, Article $article)
     {
-        $article->title = $request->title;
-        $article->body = $request->body;
+        $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit',['article' => $article]);
     }
 }

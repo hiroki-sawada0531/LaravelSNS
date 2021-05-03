d8ccd8871ff97323967536b944b4dc483c6eefaa
澤田大樹 <hiroki.bigtree@icloud.com>
2021-05-03 16:47:19 +0900

    コントローラーに記事登録処理としてstoreアクションメソッドを追加

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Article;
 
+use App\Http\Requests\ArticleRequest;
+
 class ArticleController extends Controller
 {
     public function index() {
@@ -17,5 +19,14 @@ class ArticleController extends Controller
 
     public function create() {
         return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }

fb0675d409d3fce810eee3202a0b086e7a33bb02
澤田大樹 <hiroki.bigtree@icloud.com>
2021-05-05 15:37:03 +0900

    ポリシーをコントローラーで使用する

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -10,6 +10,10 @@ use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class,'article');
+    }
     public function index() {
 
         $articles = Article::all()->sortByDesc('created_at');

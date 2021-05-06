addc6b6b168d37ea618fd4e5f7702c35557eb42f
澤田大樹 <hiroki.bigtree@icloud.com>
2021-05-06 13:50:16 +0900

    いいね機能のVueコンポーネントを作成

---
+++ laravel/resources/js/components/ArticleLike.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <button type="button" class="btn m-0 p-1 shadow-none">
+            <i class="fas fa-heart mr-1"></i>
+        </button>
+        10
+    </div>
+</template>

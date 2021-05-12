71ee8e6e878ce04492abc13ec5e1f2823ff8729c
澤田大樹 <hiroki.bigtree@icloud.com>
2021-05-12 21:38:09 +0900

    タグモデルでタグ名の保存を許可する

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Article;
 
+use App\Tag;
+
 use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
@@ -30,6 +32,11 @@ class ArticleController extends Controller
         $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
+
+        $request->tags->each(function($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
         return redirect()->route('articles.index');
     }
 
